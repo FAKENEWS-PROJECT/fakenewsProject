88ddba6b068ce57ab0340de3315c486c4e41fcb4
MiCherex <mahmudov_0818@1511.ru>
2021-12-03 21:37:23 +0300

    .

--- fakeNews/main.py
+++ fakeNews/main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
+import json
+from urllib.parse import urlparse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+link = str(input())
+req = requests.get(link).text
+soup = BeautifulSoup(req, "lxml")
+head = soup.find('head')
+zagolovok = head.find('title').text
+textp = soup.find_all('div', attrs={'class':"article__text"})
+if (len(textp))==0:
+    textp = soup.find_all('span', attrs={'class':'intro'})
+if (len(textp))==0:
+    text = soup.find_all('span', attrs={'class': 'intro'})
+if(len(textp))==0:
+    textp = soup.find_all('p')
+data = soup.find_all('time')
+if len(data)==0:
+    data = soup.find_all('span', attrs={'class': 'article__info-date-modified'})
 
+print(zagolovok)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+for i in range(len(textp)):
+    print(textp[i].text)
+if len(data)!=0:
+    for i in range(len(data)):
+        print(data[i].text)
+else:
+    print("дата публикации на новстоном ресусре не указана")
 
+domain = urlparse(link).netloc
+if (int(domain.find("www."))!=-1):
+    domain = domain[4:]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+url = link.split("//")[-1].split("/")[0].split('?')[0]
+show = "https://input.payapi.io/v1/api/fraud/domain/age/" + domain
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+r = requests.get(show)
+soupp = BeautifulSoup(r.text, "lxml")
+
+if r.status_code == 200:
+
+    age = soupp.find('body').text
+    str = int(age.find("Reg"))
+    end= int(age.find("}"))
+    print (age[str:(end-1)])
+
+    data = r.text
